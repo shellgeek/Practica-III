5a25ec8cde84cb84ac0b6fe75e4dc38daac0c7bc
shellgeek <42349304+shellgeek@users.noreply.github.com>
2020-09-11 22:50:50 -0300

    Ya casi

--- P9/Program.cs
+++ P9/Program.cs
@@ -1,4 +1,7 @@
-﻿/* El cajero debe solicitar Banco y monto a retirar. Si el banco es ABC el limite de retiro son
+﻿/*
+Cree una aplicación de cajero automático para el banco ABC. El cajero tendrá un límite
+de billetes descrito a continuación: 9 de 1000, 19 de 500, 99 de 100
+El cajero debe solicitar Banco y monto a retirar. Si el banco es ABC el limite de retiro son
 10,000 y 2000 pesos por transacción en caso contrario. El cajero debe informar si el
 monto solicitado no puede ser dispensado o si excede el límite de transacción. Y debe
 hacer la distribución de los billetes de acuerdo al monto. Por ejemplo, si el usuario
@@ -14,7 +17,21 @@ namespace P9
     {
         static void Main(string[] args)
         {
-         
+         int Bill1000 = 9, Bill500 = 19, Bill100 = 99,  cant=0;
+            Console.WriteLine("digite el monto a separar:");
+            cant = Convert.ToInt16(Console.ReadLine());
+            Bill1000 = cant / 1000;
+            cant = cant - (Bill1000 * 1000);
+            Bill500 = cant / 500;
+            cant = cant - (Bill500 * 500);
+            Bill100 = cant / 100;
+            cant = cant - (Bill100 * 100);
+           
+            Console.WriteLine("billetes de 1000:" + Bill1000);
+            Console.WriteLine("billetes de 500:" + Bill500);
+            Console.WriteLine("billetes de 100:" + Bill100);
+            Console.ReadKey();
+
         }
     }
 }
